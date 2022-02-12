3fcb51202b103a640827a261494ce22f735c22cb
Mobin <Mobinatashi2@gmail.com>
2022-02-12 21:01:08 +0330

    configure django

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from customers.models import Costumers, Addresses
+
+admin.site.register(Costumers)
+admin.site.register(Addresses)
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from products.models import Products, Categories, Discount
+
+admin.site.register(Products)
+admin.site.register(Categories)
+admin.site.register(Discount)
