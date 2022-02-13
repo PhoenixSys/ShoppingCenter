6d6c074ed2df1954aa5bc8c697dedc01fd4ee49b
Mobin <Mobinatashi2@gmail.com>
2022-02-13 12:19:44 +0330

    configure django

--- products/models.py
+++ products/models.py
@@ -1,6 +1,5 @@
 # Create your models here.
 from django.db import models
-
 from core.models import BaseModel
 
 
@@ -12,13 +11,26 @@ class Products(BaseModel):
     image = models.ImageField(upload_to="uploads/")
     description = models.TextField()
 
+    def __str__(self):
+        return f"{self.name} | {self.category} , {self.price}"
 
+
+#
 class Categories(BaseModel):
     type = models.CharField(max_length=64)
-    discount = models.ForeignKey("Discount", on_delete=models.RESTRICT)
+    discount = models.ForeignKey("Discount", on_delete=models.RESTRICT, null=True, blank=True)
+    parent = models.ForeignKey(
+        "Categories", related_name="categories", null=True, blank=True, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"type : {self.type} | parent : {self.parent}"
 
 
 class Discount(BaseModel):
     type = models.CharField(max_length=64)
     value = models.IntegerField()
     code = models.CharField(max_length=64, null=True, blank=True)
+
+    def __str__(self):
+        return f"type : {self.type} | value : {self.value} | code : {self.code}"
