755860b96edd000b2eaa5a5ab8af669579a182f2
Mobin <Mobinatashi2@gmail.com>
2022-02-13 22:02:19 +0330

    working on templates

--- static/_layout/js/_navbar.js
+++ static/_layout/js/_navbar.js
@@ -13,29 +13,5 @@
             }
         });
     });
-    $(document).ready(function () {
-        $('body.hero-anime').removeClass('hero-anime');
-    });
-    $('body').on('mouseenter mouseleave', '.nav-item', function (e) {
-        if ($(window).width() > 750) {
-            var _d = $(e.target).closest('.nav-item');
-            _d.addClass('show');
-            setTimeout(function () {
-                _d[_d.is(':hover') ? 'addClass' : 'removeClass']('show');
-            }, 1);
-        }
-    });
-
-    //Switch light/dark
-
-    $("#switch").on('click', function () {
-        if ($("body").hasClass("dark")) {
-            $("body").removeClass("dark");
-            $("#switch").removeClass("switched");
-        } else {
-            $("body").addClass("dark");
-            $("#switch").addClass("switched");
-        }
-    });
 
 })(jQuery);
