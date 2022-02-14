9bc91769ece72ecf5eecb33f6a65595a055e3c6f
Mobin <Mobinatashi2@gmail.com>
2022-02-14 20:33:55 +0330

    working on templates

--- customers/views.py
+++ customers/views.py
@@ -6,7 +6,7 @@ from django.views import View
 
 class TestTemplates(View):
     def get(self, request):
-        return render(request, "_layout/_cart.html")
+        return render(request, "_layout/cards.html")
 
 class TestTemplates1(View):
     def get(self, request):
