fc8d3028ba9c4a97d30f2695dc7b6b3cd1d513f8
Mobin <Mobinatashi2@gmail.com>
2022-02-15 22:21:14 +0330

    working on views

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from products.views import ProductsView
 
 urlpatterns = [
-    path('', ProductsView.as_view(), name="products"),
+    path('', ProductsView.as_view(), name="home"),
 
 ]
