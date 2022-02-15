e7163691fb1ccfe34b0b8598c059b15e41c4501b
Mobin <Mobinatashi2@gmail.com>
2022-02-15 14:31:47 +0330

    working on views

--- ShoppingCenter/urls.py
+++ ShoppingCenter/urls.py
@@ -20,7 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
+                  path('', include("products.urls")),
                   path('customers/', include("customers.urls")),
-                  # path('orders/', include("orders.urls")),
-                  # path('products/', include("products.urls")),
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+
+class ProductsView(View):
+    def get(self, request):
+        return render(request, "_layout/cards.html")
