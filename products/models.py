53c78918b63574e5fffbd93079b18648ab961ec2
Mobin <Mobinatashi2@gmail.com>
2022-02-16 18:47:07 +0330

    working on views

--- products/models.py
+++ products/models.py
@@ -1,15 +1,17 @@
 # Create your models here.
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 from core.models import BaseModel
 
 
 class Products(BaseModel):
-    name = models.CharField(max_length=64, unique=True)
-    category = models.ManyToManyField("Categories")
-    price = models.IntegerField()
-    discount = models.ForeignKey("Discount", on_delete=models.RESTRICT, null=True, blank=True)
-    image = models.ImageField(upload_to="uploads/", null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
+    name = models.CharField(max_length=64, unique=True, verbose_name=_("Name"))
+    category = models.ManyToManyField("Categories", verbose_name=_("Category"))
+    price = models.IntegerField(verbose_name=_("Price"))
+    discount = models.ForeignKey("Discount", on_delete=models.RESTRICT, null=True, blank=True,
+                                 verbose_name=_("Discount"))
+    image = models.ImageField(upload_to="uploads/", null=True, blank=True, verbose_name=_("Image"))
+    description = models.TextField(null=True, blank=True, verbose_name=_("Description"))
 
     def final_price(self):
         final_prices = (self.price - self.discount.profit_value(self.price)) if self.discount else self.price
@@ -21,11 +23,12 @@ class Products(BaseModel):
 
 
 class Categories(BaseModel):
-    type = models.CharField(max_length=64)
-    discount = models.ForeignKey("Discount", on_delete=models.RESTRICT, null=True, blank=True)
+    type = models.CharField(max_length=64, verbose_name=_("Type"))
+    discount = models.ForeignKey("Discount", on_delete=models.RESTRICT, null=True, blank=True,
+                                 verbose_name=_("Discount"))
     parent = models.ForeignKey(
         "Categories", related_name="categories", null=True, blank=True, on_delete=models.CASCADE
-    )
+        , verbose_name=_("Parent"))
 
     def __str__(self):
         return f"Type : {self.type} | Parent : {self.parent}"
@@ -34,9 +37,9 @@ class Categories(BaseModel):
 class Discount(BaseModel):
     type = models.CharField(max_length=10, choices=[
         ('price', 'Price'), ('percent', 'Percent')
-    ], null=False)
-    value = models.PositiveIntegerField()
-    max_price = models.PositiveIntegerField(null=True, blank=True)
+    ], null=False, verbose_name=_("Type"))
+    value = models.PositiveIntegerField(verbose_name=_("Value"))
+    max_price = models.PositiveIntegerField(null=True, blank=True, verbose_name=_("Max_Price"))
 
     def profit_value(self, price: int):
         if self.type == 'price':
