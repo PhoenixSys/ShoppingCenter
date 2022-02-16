5bf1b9b83896f2aa0ae26d3222703e156b5c3523
Mobin <Mobinatashi2@gmail.com>
2022-02-16 18:49:38 +0330

    working on views

--- customers/models.py
+++ customers/models.py
@@ -1,27 +1,27 @@
 from django.core.validators import EmailValidator
 from django.db import models
-
+from django.utils.translation import gettext_lazy as _
 # Create your models here.
 from core.models import BaseModel
 
 
 class Addresses(BaseModel):
-    costumer = models.ForeignKey("Costumers", on_delete=models.RESTRICT)
-    state = models.CharField(max_length=64)
-    city = models.CharField(max_length=64)
-    postal_code = models.IntegerField()
+    costumer = models.ForeignKey("Costumers", on_delete=models.RESTRICT, verbose_name=_("Costumer"))
+    state = models.CharField(max_length=64, verbose_name=_("State"))
+    city = models.CharField(max_length=64, verbose_name=_("City"))
+    postal_code = models.IntegerField(verbose_name=_("PostalCode"))
 
     def __str__(self):
         return f"Costumer : {self.costumer} | City : {self.city}"
 
 
 class Costumers(BaseModel):
-    username = models.CharField(max_length=64, unique=True)
-    password = models.CharField(max_length=64)
-    f_name = models.CharField(max_length=64)
-    l_name = models.CharField(max_length=64)
-    phone = models.CharField(max_length=16, unique=True)
-    email = models.CharField(max_length=64, unique=True, validators=[EmailValidator])
+    username = models.CharField(max_length=64, unique=True, verbose_name=_("Username"))
+    password = models.CharField(max_length=64, verbose_name=_("Password"))
+    f_name = models.CharField(max_length=64, verbose_name=_("First Name"))
+    l_name = models.CharField(max_length=64, verbose_name=_("Last Name"))
+    phone = models.CharField(max_length=16, unique=True, verbose_name=_("Phone"))
+    email = models.CharField(max_length=64, unique=True, validators=[EmailValidator], verbose_name=_("Email"))
 
     def __str__(self):
         return f"Username : {self.username} | Phone : {self.phone}"
