6d42db1c8886091bb306d972192ce0f5eff8301c
Mobin <Mobinatashi2@gmail.com>
2022-02-18 15:34:35 +0330

    working on categories

--- customers/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from customers.views import TestTemplates, TestTemplates1
-
-urlpatterns = [
-    path('test/', TestTemplates.as_view() , name="test1"),
-    path('test1/', TestTemplates1.as_view()),
-]
