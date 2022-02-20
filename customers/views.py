e62a642f8e586b2878943f76388f7f3a37f790a4
Mobin <Mobinatashi2@gmail.com>
2022-02-20 17:38:16 +0330

    trans _base

--- customers/views.py
+++ customers/views.py
@@ -1,5 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.messages import get_messages, add_message
+from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.views import View
 from core.models import User
@@ -17,11 +19,21 @@ class RegisterLoginView(View):
 
     def post(self, request):
         data = request.POST
-        user = User.objects.create_user(username=data["phone"], email=data["email"], password=data["password"],
-                                        phone=data["phone"])
-        costumer = Costumers.objects.create(user=user)
-        print(costumer.id)
-        return redirect("register")
+        try:
+            user = User.objects.create_user(username=data["phone"], email=data["email"], password=data["password"],
+                                            phone=data["phone"])
+            costumer = Costumers.objects.create(user=user)
+            send_mail(
+                'Congratulations !',
+                f'Dear Friend , Thanks For Join Us !\nBest Regards',
+                from_email=None,
+                recipient_list=[f"{user.email}"],
+                fail_silently=False,
+            )
+            messages.add_message(request, messages.SUCCESS, "SUCCESS")
+        except Exception as e:
+            messages.add_message(request, messages.ERROR, "ERROR")
+        return redirect("home")
 
 
 class Profile(PermissionRequiredMixin, View):
