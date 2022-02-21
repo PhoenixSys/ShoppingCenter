6d945970bc47124288441c5033f75c77e2769ce9
Mobin <Mobinatashi2@gmail.com>
2022-02-21 21:25:27 +0330

    working on get orders

---
+++ orders/serializer.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from orders.models import Order, OrderItem
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = "__all__"
+
+
+class OrderItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = "__all__"
+
+
+class ItemSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=256)
+    price = serializers.IntegerField()
+    count = serializers.IntegerField()
+
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
+from orders.models import Order, OrderItem
+from orders.serializer import OrderSerializer, OrderItemsSerializer, ItemSerializer
+
+
+class OrderApiView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        order_ser = OrderSerializer(instance=Order.objects.all(), many=True)
+        order_items_ser = OrderItemsSerializer(instance=OrderItem.objects.all(), many=True)
+        content = {
+            "orders": order_ser.data,
+            "order_item": order_items_ser.data,
+            "user": request.user.id
+        }
+        return Response(content)
+
+    def post(self, request, format=None):
+        item_ser = ItemSerializer(data=request.data, many=True)
+        if item_ser.is_valid():
+            print(item_ser.data)
+        else:
+            print("is not")
+            print(item_ser.errors)
+        return Response({})
