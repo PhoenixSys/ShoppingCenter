ab194646931e827145a192289df8e25e5e922c7c
Mobin <Mobinatashi2@gmail.com>
2022-02-21 13:13:20 +0330

    complete costumer test

--- customers/tests.py
+++ customers/tests.py
@@ -1,14 +1,23 @@
 from django.test import TestCase
 
 # Create your tests here.
+from core.models import User
 from customers.models import Costumers
 
 
-class TestCustomer(TestCase):
+class TestCostumer(TestCase):
     def setUp(self):
-        self.costumer1 = Costumers.objects.create(username="phoenix", password="1234", f_name="mobin", l_name="atashi",
-                                                  phone="626426246", email="mobinatashi2@gmail.com")
+        self.user1 = User.objects.create_user(phone="09142520208", password="admin", email="mobinatashi2@gmail.com")
+        self.costumer1 = Costumers.objects.create(user=self.user1)
 
-    def test1(self):
-        self.assertEqual(self.costumer1.is_active, True)
-        self.assertEqual(self.costumer1.is_deleted, False)
+    def test_user(self):
+        self.assertEqual(self.costumer1.user.username, self.costumer1.user.phone)
+        self.assertIn(self.costumer1.user.id, [1, 2, 3])
+        self.assertEqual(self.costumer1.user.id, 1)
+        self.assertEqual(self.costumer1.user.phone, "09142520208")
+        self.assertEqual(self.costumer1.user.email, "mobinatashi2@gmail.com")
+        self.assertNotEqual(self.costumer1.user.password, "admin")
+
+    def test_costumer(self):
+        self.assertIsNotNone(self.costumer1.id)
+        self.assertIsNone(self.costumer1.default_address)
