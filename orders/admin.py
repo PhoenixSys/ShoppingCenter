844ca2984db0579988419de70f4c95af5fb218d8
Mobin <Mobinatashi2@gmail.com>
2022-02-25 16:27:40 +0330

    working on temp

--- orders/admin.py
+++ orders/admin.py
@@ -4,9 +4,15 @@ from django.contrib import admin
 from orders.models import OrderItem, Order
 
 
-class CustomAdmin(admin.ModelAdmin):
-    search_fields = ["id"]
+class CustomAdminOrder(admin.ModelAdmin):
+    search_fields = ["id", "costumer", "status"]
+    list_display = ["id", "costumer", "status", "is_paid"]
 
 
-admin.site.register(Order, CustomAdmin)
-admin.site.register(OrderItem, CustomAdmin)
+class CustomAdminOrderItem(admin.ModelAdmin):
+    search_fields = ["id", "order", "item", "quantity"]
+    list_display = ["id", "order", "item", "quantity"]
+
+
+admin.site.register(Order, CustomAdminOrder)
+admin.site.register(OrderItem, CustomAdminOrderItem)
