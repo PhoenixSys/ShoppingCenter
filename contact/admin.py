6d88703162739486a423c3750f93113d8f32bf39
Mobin <Mobinatashi2@gmail.com>
2022-02-26 19:07:50 +0330

    fix some bugs

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from contact.models import ContactUs
 
-admin.site.register(ContactUs)
+
+class CustomAdminContactUs(admin.ModelAdmin):
+    search_fields = ["id", "name", "email", "text"]
+    list_display = ["id", "name", "email", "text"]
+
+
+admin.site.register(ContactUs, CustomAdminContactUs)
--- contact/models.py
+++ contact/models.py
@@ -2,9 +2,14 @@ from django.db import models
 
 # Create your models here.
 from core.models import BaseModel
+from django.utils.translation import gettext_lazy as _
 
 
 class ContactUs(BaseModel):
     name = models.CharField(max_length=64)
     email = models.EmailField()
     text = models.TextField()
+
+    class Meta:
+        verbose_name = _("ContactUs")
+        verbose_name_plural = _("ContactUs")
