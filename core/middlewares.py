9ad9dce1e3b363fb2a9bbe947600b5349c5fe041
Mobin <Mobinatashi2@gmail.com>
2022-03-01 22:43:29 +0330

    ban user with ip

--- core/middlewares.py
+++ core/middlewares.py
@@ -6,12 +6,9 @@ from core.models import IpManagerDb
 class IpManager:
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
         import requests
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
         request_ip = requests.get("https://api.ipify.org?format=json").json()["ip"]
         IpManagerDb.objects.get_or_create(ip=request_ip)
         ip = IpManagerDb.objects.get(ip=request_ip)
