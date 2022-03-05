7576726921d8fefb550b25d6523e2f585a9bd04c
Mobin <Mobinatashi2@gmail.com>
2022-03-05 14:45:01 +0330

    start working on dockerize

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,12 @@
 FROM python
 ENV PYTHONDONTWRITEBYTECODE=1
 ENV PYTHONUNBUFFERED=1
-WORKDIR /ariana
-COPY requirements.txt /ariana/
+WORKDIR /code
+COPY requirements.txt /code/
+RUN pip install -U pip
 RUN pip install -r requirements.txt
 COPY . /ariana/
+
+EXPOSE 8000
+
+CMD ["gunicorn" , "ShoppingCenter.wsgi" , ":8000"]
--- ShoppingCenter/settings.py
+++ ShoppingCenter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -92,8 +93,12 @@ WSGI_APPLICATION = 'ShoppingCenter.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_NAME'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 # rest settings
