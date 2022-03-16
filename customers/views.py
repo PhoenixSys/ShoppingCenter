7b6a0199a5ca1dc66047b0d7bfcb0a6ac2b7b23b
Mobin <Mobinatashi2@gmail.com>
2022-03-16 19:32:23 +0330

    working on django messages

--- customers/views.py
+++ customers/views.py
@@ -3,49 +3,53 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.contrib.auth.models import Permission
 from django.contrib.contenttypes.models import ContentType
-from django.contrib.messages import get_messages, add_message
+from django.contrib.messages import get_messages
 from django.core.mail import send_mail
 from django.shortcuts import render, redirect
 from django.views import View
 from core.models import User
 from customers.models import Costumers, Addresses
 from orders.models import Order
-from products.models import Categories
+import re
 
 
 class RegisterLoginView(View):
     def get(self, request):
-        categories = Categories.objects.all()
-        context = {
-            "categories": categories
-        }
-        return render(request, "customers/register_login_page.html", context=context)
+        return render(request, "customers/register_login_page.html")
 
     def post(self, request):
         data = request.POST
         try:
-            user = User.objects.create_user(username=data["phone"], email=data["email"], password=data["password"],
-                                            phone=data["phone"])
-            content = ContentType.objects.get_for_model(User)
-            Permission.objects.get_or_create(codename="authenticated", name="Can See Profile",
-                                             content_type=content)
-            perm = Permission.objects.get(codename="authenticated")
-            user.user_permissions.add(perm)
-            user.save()
-            Costumers.objects.create(user=user)
-            send_mail(
-                'Congratulations !',
-                f'Dear Friend , Thanks For Join Us !\nBest Regards',
-                from_email=None,
-                recipient_list=[f"{user.email}"],
-                fail_silently=False,
-            )
-            user_logged = authenticate(username=user.username, password=data["password"])
-            login(request, user_logged)
-            messages.add_message(request, messages.SUCCESS, "SUCCESS")
+            pattern_email = "[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+            email_validator = re.fullmatch(pattern_email, data["email"])
+            pattern_phone = "^(9|(09))(((1)|(3))([0-9])|(20)|(21))(\d{7})"
+            phone_validator = re.fullmatch(pattern_phone, data["phone"])
+            if email_validator and phone_validator:
+                user = User.objects.create_user(username=data["phone"], email=data["email"], password=data["password"],
+                                                phone=data["phone"])
+                content = ContentType.objects.get_for_model(User)
+                Permission.objects.get_or_create(codename="authenticated", name="Can See Profile",
+                                                 content_type=content)
+                perm = Permission.objects.get(codename="authenticated")
+                user.user_permissions.add(perm)
+                user.save()
+                Costumers.objects.create(user=user)
+                send_mail(
+                    'Congratulations !',
+                    f'Dear Friend , Thanks For Join Us !\nBest Regards',
+                    from_email=None,
+                    recipient_list=[f"{user.email}"],
+                    fail_silently=False,
+                )
+                user_logged = authenticate(username=user.username, password=data["password"])
+                login(request, user_logged)
+                messages.add_message(request, messages.SUCCESS, "You Are Successfully Registered !")
+            else:
+                messages.add_message(request, messages.ERROR, "Register Failed ! Please Check Email/Phone Values")
         except Exception as e:
-            messages.add_message(request, messages.ERROR, "ERROR")
-        return redirect("home")
+            messages.add_message(request, messages.ERROR, "Register Failed ! Please Check Input's Data")
+        message = get_messages(request)
+        return render(request, "customers/register_login_page.html", context={"messages": message})
 
 
 class Profile(PermissionRequiredMixin, View):
