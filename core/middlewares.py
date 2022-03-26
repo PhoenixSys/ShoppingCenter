e96e5e92f97bdd7ce34d1f4e9b2636e2e1030847
Mobin <Mobinatashi2@gmail.com>
2022-03-26 11:33:47 +0430

    improve ip manager

--- core/middlewares.py
+++ core/middlewares.py
@@ -12,18 +12,26 @@ class IpManager:
         try:
             request_ip = requests.get("https://api.ipify.org?format=json").json()["ip"]
             resp = requests.get(
-                f"http://ip-api.com/json/{request_ip}?fields=status,message,country,city,lat,lon").json()
+                f"http://ip-api.com/json/{request_ip}?fields=status,message,country,city,lat,lon,proxy").json()
             country = resp["country"]
             city = resp["city"]
             lat = resp["lat"]
             lon = resp["lon"]
-            IpManagerDb.objects.get_or_create(ip=request_ip, country=country, city=city, lat=lat, lon=lon)
-            ip = IpManagerDb.objects.get(ip=request_ip)
-            if (ip.access is True) and (ip.country == "Iran" or ip.country == "Netherlands"):
-                ip.views += 1
-                ip.save()
-                response = self.get_response(request)
-                return response
+            proxy = resp["proxy"]
+            if not proxy:
+                IpManagerDb.objects.get_or_create(ip=request_ip, country=country, city=city, lat=lat, lon=lon)
+                ip = IpManagerDb.objects.get(ip=request_ip)
+                if (ip.access is True) and (ip.country == "Iran" or ip.country == "Netherlands"):
+                    ip.views += 1
+                    ip.save()
+                    response = self.get_response(request)
+                    return response
+                else:
+                    context = {
+                        "ip": request_ip,
+                        "region": country
+                    }
+                    return render(request, "_layout/access_denied.html", context=context)
             else:
                 context = {
                     "ip": request_ip,
